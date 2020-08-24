cef65dd38db326a75c1622299946df40a12c6d7c
DogJun <498343874@qq.com>
2020-08-24 12:15:51 +0800

    feat: add 206

---
+++ 206.反转链表.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let next = head
+  let pre = null
+  while (next) {
+    [next.next, pre, next] = [pre, next, next.next]
+  }
+  return pre
+};
+// @lc code=end
+
